178676db89d89bace584db591788325294342ee0
EdisonAlvarezBa <edisonalvarezbarreto@gmail.com>
2026-03-13 11:29:57 -0500

    agregando  analisis.py

---
+++ analisis.py
@@ -0,0 +1,14 @@
+dataset=[10,25,37,42,58,63,71,89]
+
+def promedio(datos):
+    return sum(datos)/len(datos) 
+
+def maximo(datos):
+    return max(datos)
+
+def resumen(datos):
+    print(f"Total de ingresos: {len(datos)}")
+    print(f"promedio         : {promedio(datos):.2f}")
+    print(f"Máximo           : {maximo(datos)}")
+    
+resumen(dataset)
